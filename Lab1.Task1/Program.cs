ccec7020e375e75a6888b65a4da50d1715aa5d61
Vladislav <sodaluvfunboy@gmail.com>
2024-04-28 10:25:09 +0300

    Add comment and readme

---
+++ Lab1.Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab1.Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Comment
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
